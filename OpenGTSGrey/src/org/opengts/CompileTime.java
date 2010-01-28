0fc22cba5e50063e42a5f55a0898d80571881a8a
ruben <ruben@404fs.com>
2010-01-28 01:24:23 +0000

    updated options

--- OpenGTSGrey/src/org/opengts/CompileTime.java
+++ OpenGTSGrey/src/org/opengts/CompileTime.java
@@ -1,6 +1,6 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2010/01/26 14:10:26 CET
-    public static final long COMPILE_TIMESTAMP = 1264511426L;
+    // 2010/01/28 02:17:35 CET
+    public static final long COMPILE_TIMESTAMP = 1264641455L;
 }
